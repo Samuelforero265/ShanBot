8e08f9a81cd9b19f755aa130abeb551ef339d73f
Samuelforero265 <87767388+Samuelforero265@users.noreply.github.com>
2021-07-21 11:36:18 -0500

    Update help.js

--- src/help.js
+++ src/help.js
@@ -1,11 +1,11 @@
 const help = (prefix) => {
 	return `
 
-⌜ *ѕнαηвσт ву ѕнαη∂υу* ⌟  
+⌜ *AnnSamBot ву ѕamuelGod* ⌟  
 
 ◉ *INFORMACION*
    ○ Comando: ⌜ ${prefix} ⌟
-   ○ Creador: ѕнαη∂υу™ 
+   ○ Creador: SamuelGod
    ○ Como instalar el bot: https://www.youtube.com/watch?v=2LQSzEbpJ-M
    ○ Instagram: https://www.instagram.com/thepavos
 
@@ -92,7 +92,7 @@ Para activar los NSFW coloque el siguiente comando ${prefix}nsfw 1 y para desact
 No te olvides de seguirme en instagram flaco ;)
 
 
-ву ѕнαη∂υу
+ву SamuelGod
 
 
 ⌜ *ごきげんよう :)* ⌟ 
